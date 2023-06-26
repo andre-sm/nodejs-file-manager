31d99156e85912d94c27039b03fdd807dbd05c26
--global <--global>
2023-06-26 22:10:14 +0300

    feat: add try and catch blocks

--- src/app.js
+++ src/app.js
@@ -6,69 +6,73 @@ import { getArgs } from './utils/get-args.js';
 import { getOsInfo } from './commands/os/os-info.js';
 
 const startApp = async () => {
-    const userName = getArgs('username') || 'Guest';
+    try {
+        const userName = getArgs('username') || 'Guest';
 
-    console.log(`Welcome to the File Manager, ${userName}!`);
-    directoryCommands.printCurrentDirectory();
-    console.log('Please write a command');
+        console.log(`Welcome to the File Manager, ${userName}!`);
+        directoryCommands.printCurrentDirectory();
+        console.log('Please write a command');
 
-    process.on('SIGINT', () => {
-        closeProcess(userName);
-    });
-    
-    const readLine = readline.createInterface({ input: stdin });
+        process.on('SIGINT', () => {
+            closeProcess(userName);
+        });
+        
+        const readLine = readline.createInterface({ input: stdin });
 
-    for await (const line of readLine) {
-        const hasQuotes = checkForQuotes(line);
-        const commandArray = splitCommand(line, hasQuotes);
+        for await (const line of readLine) {
+            const hasQuotes = checkForQuotes(line);
+            const commandArray = splitCommand(line, hasQuotes);
 
-        switch (commandArray[0]) {
-            case '.exit':
-                closeProcess(userName);
-                break;
-            case 'up':
-                await directoryCommands.moveUP();
-                break;
-            case 'cd':
-                await directoryCommands.goToDirectory(commandArray[1]);
-                break;
-            case 'ls':
-                await directoryCommands.readDirectory();
-                break;
-            case 'cat':
-                await fileCommands.readFile(commandArray[1]);
-                break;
-            case 'add':
-                await fileCommands.createFile(commandArray[1]);
-                break;
-            case 'rn':
-                await fileCommands.renameFile(commandArray[1], commandArray[2]);
-                break;
-            case 'cp':
-                await fileCommands.copyFile(commandArray[1], commandArray[2]);
-                break;
-            case 'mv':
-                await fileCommands.moveFile(commandArray[1], commandArray[2]);
-                break;
-            case 'rm':
-                await fileCommands.deleteFile(commandArray[1]);
-                break;
-            case 'os':
-                await getOsInfo(commandArray[1].slice(2));
-                break;  
-            case 'hash':
-                await fileCommands.calculateHash(commandArray[1]);
-                break;  
-            case 'compress':
-                await fileCommands.compressFile(commandArray[1], commandArray[2]);
-                break;
-            case 'decompress':
-                await fileCommands.decompressFile(commandArray[1], commandArray[2]);
-                break;  
-            default:
-                console.error('Invalid input');
-                directoryCommands.printCurrentDirectory();
+            switch (commandArray[0]) {
+                case '.exit':
+                    closeProcess(userName);
+                    break;
+                case 'up':
+                    await directoryCommands.moveUP();
+                    break;
+                case 'cd':
+                    await directoryCommands.goToDirectory(commandArray[1]);
+                    break;
+                case 'ls':
+                    await directoryCommands.readDirectory();
+                    break;
+                case 'cat':
+                    await fileCommands.readFile(commandArray[1]);
+                    break;
+                case 'add':
+                    await fileCommands.createFile(commandArray[1]);
+                    break;
+                case 'rn':
+                    await fileCommands.renameFile(commandArray[1], commandArray[2]);
+                    break;
+                case 'cp':
+                    await fileCommands.copyFile(commandArray[1], commandArray[2]);
+                    break;
+                case 'mv':
+                    await fileCommands.moveFile(commandArray[1], commandArray[2]);
+                    break;
+                case 'rm':
+                    await fileCommands.deleteFile(commandArray[1]);
+                    break;
+                case 'os':
+                    await getOsInfo(commandArray[1].slice(2));
+                    break;  
+                case 'hash':
+                    await fileCommands.calculateHash(commandArray[1]);
+                    break;  
+                case 'compress':
+                    await fileCommands.compressFile(commandArray[1], commandArray[2]);
+                    break;
+                case 'decompress':
+                    await fileCommands.decompressFile(commandArray[1], commandArray[2]);
+                    break;  
+                default:
+                    console.error('Invalid input');
+                    directoryCommands.printCurrentDirectory();
+            }
         }
+    } catch (err) {
+        console.error(err.message);
     }
 };
 
--- src/commands/os/os-info.js
+++ src/commands/os/os-info.js
@@ -2,35 +2,39 @@ import { EOL, cpus, homedir, userInfo, arch } from 'os';
 import * as directoryCommands from '../directory/index.js';
 
 export const getOsInfo = async (arg) => {
-    switch (arg) {
-        case 'EOL':
-            console.log(JSON.stringify(EOL));
-            directoryCommands.printCurrentDirectory();
-            break;
-        case 'cpus':
-            const cpusData = cpus();
-            const cpusInfo = [`Amount of CPUS: ${cpusData.length}`];
-            cpusData.forEach((cpu, i) => {
-                const speed = (cpu.speed / 1000).toFixed(3);
-                cpusInfo.push(`CPU${i + 1} model: ${cpu.model}, clock rate: ${speed} GHz`);
-            });
-            console.log(cpusInfo.join('\n'));
-            directoryCommands.printCurrentDirectory();
-            break;
-        case 'homedir':
-            console.log(`Home directory: ${homedir()}`);
-            directoryCommands.printCurrentDirectory();
-            break;
-        case 'username':
-            console.log(`Current system user name: ${userInfo().username}`);
-            directoryCommands.printCurrentDirectory();
-            break;
-        case 'architecture':
-            console.log(`CPU architecture: ${arch()}`);
-            directoryCommands.printCurrentDirectory();
-            break;
-        default:
-            console.error('Invalid input');
-            directoryCommands.printCurrentDirectory();
+    try {
+        switch (arg) {
+            case 'EOL':
+                console.log(JSON.stringify(EOL));
+                directoryCommands.printCurrentDirectory();
+                break;
+            case 'cpus':
+                const cpusData = cpus();
+                const cpusInfo = [`Amount of CPUS: ${cpusData.length}`];
+                cpusData.forEach((cpu, i) => {
+                    const speed = (cpu.speed / 1000).toFixed(3);
+                    cpusInfo.push(`CPU${i + 1} model: ${cpu.model}, clock rate: ${speed} GHz`);
+                });
+                console.log(cpusInfo.join('\n'));
+                directoryCommands.printCurrentDirectory();
+                break;
+            case 'homedir':
+                console.log(`Home directory: ${homedir()}`);
+                directoryCommands.printCurrentDirectory();
+                break;
+            case 'username':
+                console.log(`Current system user name: ${userInfo().username}`);
+                directoryCommands.printCurrentDirectory();
+                break;
+            case 'architecture':
+                console.log(`CPU architecture: ${arch()}`);
+                directoryCommands.printCurrentDirectory();
+                break;
+            default:
+                console.error('Invalid input');
+                directoryCommands.printCurrentDirectory();
+        }
+    } catch (err) {
+        console.error(err.message);
     }
-}
+};
